043ce83d8a2bddce2cea7cdbabe7349ce505f006
kyad <53517611+kyad@users.noreply.github.com>
2023-08-18 23:35:58 +0900

    Update init

--- emacs/init.el
+++ emacs/init.el
@@ -250,14 +250,25 @@ document.addEventListener('DOMContentLoaded', () => {
   (persistent-scratch-setup-default))
 
 (set-face-font 'default "migu 1m-12")
-(setq compile-command "g++ -g3 -I ${HOME}/ac-library -Wall a.cc")
+(setq compile-command "g++ -g3 -O0 -I ${HOME}/ac-library -Wall a.cc")
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(blackout el-get hydra leaf-keywords leaf)))
+ '(indent-tabs-mode nil)
+ '(inhibit-startup-message t)
+ '(initial-scratch-message "")
+ '(line-number-mode t)
+ '(menu-bar-mode nil)
+ '(package-archives
+   '(("gnu" . "https://elpa.gnu.org/packages/")
+     ("melpa" . "https://melpa.org/packages/")
+     ("org" . "https://orgmode.org/elpa/")))
+ '(package-selected-packages '(blackout el-get hydra leaf-keywords leaf))
+ '(scroll-bar-mode nil)
+ '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
